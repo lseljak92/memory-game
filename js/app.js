e74ebb61bcc05570d164d97040893517df47bb2f
Maria Seljak <lseljak92@gmail.com>
2018-07-12 12:07:16 -0800

    fix: Deck event listener fixed

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ let timerOff = true;
 let time = 0;
 let timer;
 let cardsMatched = 0;
-const CARDS_PAIR = 2;
 const TOTAL_MATCHES = 8;
 const exitButton = document.querySelector('.modal-exit');
 const replayButton = document.querySelector('.modal-replay');
@@ -36,29 +35,31 @@ function shuffle(array) {
 //Event listener for a card
 deck.addEventListener('click', event => {
     const clickTarget = event.target;
-    if(validClick(clickTarget)
+    if(validClick(clickTarget)   
     ){
         if(timerOff){
             startTimer();
             timerOff = false;
         }
-    }
-    toggleCard(clickTarget);
-    addToggleCard(clickTarget);
-    if(toggledCards.length === CARDS_PAIR){
-        checkPair(clickTarget);
-        incrementMove();
-        checkScore();
+        toggleCard(clickTarget);
+        addToggleCard(clickTarget);
+        if(toggledCards.length === 2){
+            checkPair(clickTarget);
+            incrementMove();
+            checkScore();
+        }
     }
 });
+
 
 //Function to avoid multiple cards from being open
 function validClick(clickTarget){
     return(
-        clickTarget.classList.contains('card')&&
-        !clickTarget.classList.contains('match')&&
+        clickTarget.classList.contains('card') && 
+        !clickTarget.classList.contains('match') &&
         toggledCards.length < 2 &&
         !toggledCards.includes(clickTarget)
+    
     );
 }
 
@@ -71,6 +72,7 @@ function toggleCard(card){
 //Store toggled cards into array
 function addToggleCard(clickTarget){
     toggledCards.push(clickTarget);
+    console.log(toggledCards);
 }
 
 //Compare card elements to establish if it is a match
@@ -80,17 +82,17 @@ function checkPair(){
         toggledCards[1].firstElementChild.className)
         {
         toggledCards[0].classList.toggle('match');
-        toggledCards[1].classList.toggle('match'); 
+        toggledCards[1].classList.toggle('match');
         toggledCards = [];
-        cardsMatched++;  
-        winGame(); 
-        }else{
-            setTimeout(() => {
-                toggleCard(toggledCards[0]);
-                toggleCard(toggledCards[1]);
-                toggledCards = [];
-            },  1000);
-        }
+        cardsMatched++;
+        winGame();
+    }else{
+        setTimeout(() => {
+            toggleCard(toggledCards[0]);
+            toggleCard(toggledCards[1]);
+            toggledCards = [];
+        },  1000);
+    }
 }
 
 function incrementMove(){
@@ -124,10 +126,11 @@ function winGame(){
     }
 }
 
-function startClock(){
+function startTimer(){
     timer = setInterval(() => {
         time++;
         displayTime();
+        console.log(time);
     },  1000);
 }
 
@@ -215,13 +218,3 @@ function resetDeck(){
         card.className = 'card';
     }
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
